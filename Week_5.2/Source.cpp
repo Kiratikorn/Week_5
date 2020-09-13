0f52853cdbe70f9aa0d4d03ac9a6611d4222ec1a
KIRATIKORN PHLAPPLA <63010086@KMITL.AC.TH>
2020-09-13 21:09:35 +0700

    5.2change

--- Week_5.2/Source.cpp
+++ Week_5.2/Source.cpp
@@ -12,25 +12,9 @@ int main()
 			{
 				printf("* ");
 			}
-			for (int j = 0; j < -4 * i + 4 * n - 4; j++)
-			{
-				printf(" ");
-			}
-			for (int j = 0; j <= i; j++)
-			{
-				printf("* ");
-			}
 		}
 		else
 		{
-			for (int j = 0; j < n - (i - n) - 1; j++)
-			{
-				printf("* ");
-			}
-			for (int j = 0; j < 4 * (i - n + 1); j++)
-			{
-				printf(" ");
-			}
 			for (int j = 0; j < n - (i - n) - 1; j++)
 			{
 				printf("* ");
